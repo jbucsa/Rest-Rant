5584f8be9c382bd1ef2a60bb0f634aa8306cfa1e
jbucsa <justin.bucsa@gmail.com>
2023-10-24 19:17:06 -0500

    Added list to index.js file

---
+++ index.js
@@ -0,0 +1,25 @@
+//Check List
+
+//1. Create Project Folder
+//2. Creating package.json
+//3. Create package.json file
+//3a   In the terminal type: npm init 
+//3b. Complete additional steps for package.json document. 
+//3(z). Once package.json is complete, type: yes.
+// The package.json is not complete.
+//5. Create index.js file
+// type: touch index.js
+// Javascript file is complete.
+//6 Create HTML file
+// type: touch index.html
+//7. Require express at the top of the file
+
+// 8. Initialize the app variable
+
+//9. Create the home page route
+//9a. Call app.get()
+//9b. Set ‘/‘ as the path (first arg)
+//9c. Write callback function with req, res
+//9d. Call res.send(‘hello world’) 
+
+//10. Call app.listen(3000) to keep server open
