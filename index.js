b764998f3c17f6aa54849cbc9f41f266467556c0
jbucsa <justin.bucsa@gmail.com>
2023-10-24 19:47:26 -0500

    Updated index.js, and package.json. Created  package-lock.json created

--- index.js
+++ index.js
@@ -23,3 +23,12 @@
 //9d. Call res.send(‘hello world’) 
 
 //10. Call app.listen(3000) to keep server open
+
+const express = require ('express')
+const app = express()
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+});
+
+app.listen(process.env.PORT)
