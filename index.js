955d98cf136b415f0dc7d232a88700eeb875f41d
jbucsa <justin.bucsa@gmail.com>
2023-10-24 20:46:55 -0500

    Adding places controller

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ const app = express();
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-app.use('/places', require('./controllers/places'));
+app.use('/places.js', require('./controllers/places.js'));
 
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('*', (req, res) => {
-  res.send('404 page');
+  res.status(404).send('<h1>404 Page</h1>');
 });
 
 app.listen(process.env.PORT);
