028b131cd95c25eeec3555dd6194d281488c9306
jbucsa <justin.bucsa@gmail.com>
2023-10-26 20:19:30 -0500

    Updated views/home.jsx file

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home(){
     return(
         <Def>
             <main>
-                <h1>Home</h1>
+                <h1>REST-Rant</h1>
                 <div>
                     <img src="/images/BeachLife.jpg" alt="Image of Beach Life" />
                 </div>
