01fefe951378a4268fcf588d6263b4395364b7d4
jbucsa <justin.bucsa@gmail.com>
2023-10-31 20:31:51 -0500

    Show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,14 +3,17 @@ const Def = require('../default');
 
 // Define another React component
 function index(data) {
-  const placesFormatted = data.places.map((place, index) => (
+  let placesFormatted = data.places.map((place, index) => {
+    return (
     <div className="col-sm-6">
-      <h2>{place.name}</h2>
+      <h2>
+        <a href={`/places/${index}`} >{place.name}</a>
+      </h2>
       <p className="text-left">{place.cuisines}</p>
       <img src={place.pic} alt={place.name} />
       <p className="text-left">Located in {place.city}, {place.state}</p>
     </div>
-  ));
+  )})
 
   return (
     <Def>
