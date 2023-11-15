34e2943b02b1b77722f94c1899576989e48e3cc2
jbucsa <justin.bucsa@gmail.com>
2023-11-14 18:46:25 -0600

    Completed REST-Rant Part 10 Submission

--- controllers/places.js
+++ controllers/places.js
@@ -64,7 +64,32 @@ router.get('/:id', (req, res) => {
       console.log('err', err)
       res.render('error404')
   })
-})
+});
+
+
+//Comments
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+});
+
+
 
 //PUT Route for Edit: put the user's edits back into our places array
 router.put('/:id', (req, res) => {
