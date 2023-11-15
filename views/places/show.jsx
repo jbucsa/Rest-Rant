7e038d45f6fee26d0298e8500c61c7b53aa70b66
jbucsa <justin.bucsa@gmail.com>
2023-11-14 18:43:35 -0600

    Completed Part Three: Populate the Comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -49,7 +49,44 @@ function show (data) {
           </main>
         </Def>
     )
+
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+          </div>
+          <hr />
+          <h2>Comments</h2>
+          {comments}
+        </main>
+      </Def>
+  )
 }
+
+
+
+
+
 
 module.exports = show
 
